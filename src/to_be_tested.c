c190244b28ad43d64a5a7a0e6e10f804865ba3bd
8-Bit-Brain-Guy <>
2025-03-23 22:13:54 +0100

    1

---
+++ src/to_be_tested.c
@@ -0,0 +1,14 @@
+#include "to_be_tested.h"
+
+int add(int z1, int z2)
+{
+    return 1;
+}
+
+
+int add2(int z1, int z2)
+{
+    return (z1+z2);
+}
+
+
---
+++ src/to_be_tested.h
@@ -0,0 +1,7 @@
+#ifndef TO_BE_TESTED_H
+#define TO_BE_TESTED_H
+
+int add(int z1, int z2);
+int add2(int z1, int z2);
+
+#endif
