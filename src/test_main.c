38b1330846e88fced20cc6654d0573c066c11387
8-Bit-Brain-Guy <>
2025-03-25 22:06:37 +0100

    1

--- src/test_main.c
+++ src/test_main.c
@@ -1,6 +1,25 @@
 #include <criterion/criterion.h>
+#include <criterion/logging.h>
 #include "main.h"
 
-Test(addition, simple_add) {
-    cr_assert(add_with_limits(2, 3) == 5, "Addition failed");
+// Setup-Funktion (optional, falls nötig)
+void setup(void)
+{
+    cr_log_info("Setup: Starte Tests...\n");
+}
+
+// Teardown-Funktion (optional, falls nötig)
+void teardown(void)
+{
+    cr_log_info("Teardown: Test abgeschlossen.\n");
+}
+
+// Testfälle
+TestSuite(Addition_Tests, .init = setup, .fini = teardown);
+
+
+Test(Addition_Tests, add_with_limits)
+{
+    cr_log_info("Testen der add_with_limits function...\n");
+    cr_expect(add_with_limits(2, 3) == 5, "Expected add_with_limits of 2 and 3 = 5");
 }
